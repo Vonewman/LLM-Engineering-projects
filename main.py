f67bacb3843379f2652cb7ba329b0e3e8c1d6c6f
vonewman <vonewman7@gmail.com>
2023-08-26 17:21:37 +0000

    clone ChatGPT and create a README.md file

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import chainlit as cl
-import openai
+from openai import ChatCompletion
 
 @cl.on_message
 async def main(message: str):
-    await cl.Message(content = message).send()
+    
+    response = ChatCompletion.create(
+        model = 'gpt-4',
+        messages = [
+            {"role": "assistant", "content": "you are an assistant that think like Nietzsche"},
+            {"role": "user", "content": message}
+        ],
+        
+        temperature = 1,
+    )
+    
+    await cl.Message(content = f"{response['choices'][0]['message']['content']}", ).send()
