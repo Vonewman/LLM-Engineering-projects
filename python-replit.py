50038d68324aeac0bbf6e7c9c952663167e53c59
vonewman <vonewman7@gmail.com>
2023-08-27 16:11:41 +0000

    add python-replit.py

---
+++ python-replit.py
@@ -0,0 +1,16 @@
+from langchain.agents.agent_toolkits import create_python_agent
+from langchain.tools.python.tool import PythonREPLTool
+from langchain.python import PythonREPL
+from langchain.llms.openai import OpenAI
+from langchain.agents.agent_types import AgentType
+from langchain.chat_models import ChatOpenAI
+
+
+agent_executor = create_python_agent(
+    llm=OpenAI(temperature=0.5, max_tokens=2000),
+    tool=PythonREPLTool(),
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+)
+
+agent_executor.run("What is the factorial(n=10)?")
